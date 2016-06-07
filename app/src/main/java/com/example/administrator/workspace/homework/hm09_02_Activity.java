b7948fc497a5ae1dda5ad8e039b19dd43c57ed54
Administrator <962777741@qq.com>
2016-06-07 17:02:32 +0800

    aa

---
+++ app/src/main/java/com/example/administrator/workspace/homework/hm09_02_Activity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.workspace.homework;
+
+import android.os.Bundle;
+import android.widget.QuickContactBadge;
+
+import com.example.administrator.workspace.R;
+
+/**
+ * Created by Administrator on 2016/6/7.
+ */
+public class hm09_02_Activity extends FatherBarActivity {
+    QuickContactBadge qcb;
+    @Override
+    public void child(Bundle savedInstanceState) {
+        setContentView(R.layout.hm09_02__layout);
+        qcb= (QuickContactBadge) findViewById(R.id.badge);
+        qcb.assignContactFromPhone("18600340215",false);
+
+    }
+}
