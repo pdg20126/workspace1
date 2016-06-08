9169b76848ccb4e8d5d8ce01a1bb472449569ee8
Administrator <962777741@qq.com>
2016-06-08 11:01:21 +0800

    aa

---
+++ app/src/main/java/com/example/administrator/workspace/homework/hm_video_Activity.java
@@ -0,0 +1,34 @@
+package com.example.administrator.workspace.homework;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.example.administrator.workspace.R;
+
+import java.io.File;
+
+/**
+ * Created by Administrator on 2016/6/8.
+ */
+public class hm_video_Activity extends Activity {
+    VideoView videoView;
+    MediaController mc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hm_video_layout);
+        videoView= (VideoView) findViewById(R.id.video);
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+        mc=new MediaController(this);
+        File video=new File("/mnt/sdcard/mm.mp4");
+        if (video.exists()){
+            videoView.setVideoPath(video.getAbsolutePath());
+            videoView.setMediaController(mc);
+            mc.setMediaPlayer(videoView);
+            videoView.requestFocus();
+        }
+    }
+}
