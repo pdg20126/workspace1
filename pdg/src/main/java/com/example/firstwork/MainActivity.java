4160cdb2ca8a41eaa04875bc3692ebc8382640bf
Administrator <962777741@qq.com>
2016-06-22 18:59:21 +0800

    aa

--- pdg/src/main/java/com/example/firstwork/MainActivity.java
+++ pdg/src/main/java/com/example/firstwork/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.firstwork;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+import junit.framework.Test;
+
+public class MainActivity extends AppCompatActivity  {
+    ToggleButton tb;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+        tb= (ToggleButton) findViewById(R.id.tgbtn_show_psw);
+        et= (EditText) findViewById(R.id.edit_psw);
+        et.setInputType(0x81);
+        Toolbar t = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(t);
+        getSupportActionBar().setTitle("登录");
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+         @Override
+         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+             if (isChecked){
+                 et.setInputType(0x90);
+             }else {
+                 et.setInputType(0x81);
+             }
+         }
+     });
     }
+
 }
